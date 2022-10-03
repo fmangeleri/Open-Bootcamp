ba64156bd7c65b392389f062f551c15fb1e2ed6f
fmangeleri <98715817+fmangeleri@users.noreply.github.com>
2022-10-03 20:28:42 -0300

    Add files via upload

---
+++ Java Basico/Ejercicios Tema 1/src/com/javaBasico/ej1.java
@@ -0,0 +1,31 @@
+package com.javaBasico;
+
+
+public class ej1 {
+    public static void main(String[] args) throws Exception {
+        
+        // Creamos las variables y les asignamos un valor
+        byte numeroByte = 1;
+        short numeroShort = 15;
+        int numeroInt = 200;
+        long numeroLong = 200000;
+        float numeroFloat = 20.2f;
+        double numeroDouble = 21.4d;
+        boolean booleano = true;
+        char character = 'a';
+        String texto = "Esto es un texto";
+
+
+        // Imprimimos en la consola el valor de las variables
+        System.out.println(numeroByte);
+        System.out.println(numeroShort);
+        System.out.println(numeroInt);
+        System.out.println(numeroLong);
+        System.out.println(numeroFloat);
+        System.out.println(numeroDouble);
+        System.out.println(booleano);
+        System.out.println(character);
+        System.out.println(texto);
+
+    }
+}
