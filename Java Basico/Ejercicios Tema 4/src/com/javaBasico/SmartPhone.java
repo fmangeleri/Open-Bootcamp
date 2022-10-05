6986cbacd22c28a20db46fd3dfd5ccc7614787de
fmangeleri <98715817+fmangeleri@users.noreply.github.com>
2022-10-05 15:31:23 -0300

    Update SmartPhone.java

--- Java Basico/Ejercicios Tema 4/src/com/javaBasico/SmartPhone.java
+++ Java Basico/Ejercicios Tema 4/src/com/javaBasico/SmartPhone.java
@@ -5,12 +5,12 @@ public class SmartPhone extends SmartDevice{
     protected boolean camera;
 
     public SmartPhone(){
-        this.tipoDeDispositivo = "Smart Watch";
+        this.tipoDeDispositivo = "Smart Phone";
     }
 
     public SmartPhone(String marca, String modelo, String pantalla, boolean camera){
         super(pantalla, marca, modelo, pantalla);
         this.camera = camera;
-        this.tipoDeDispositivo = "Smart Watch";
+        this.tipoDeDispositivo = "Smart Phone";
     }
 }
