c039552a3453006fd27ac77c8792f376e0dca651
fmangeleri <98715817+fmangeleri@users.noreply.github.com>
2022-10-12 11:46:17 -0300

    Add files via upload

---
+++ JavaScript Basico/Tema 2/index.js
@@ -0,0 +1,15 @@
+const nombre = "Franco";
+const edad = 22;
+const desarrollador = true;
+const nacimiento = new Date("july 6 2000");
+const libroFav = {
+    titulo: "El Codigo Da Vinci",
+    autor: "Dan Brown",
+    publicacion: 2003,
+}
+
+console.log('nombre: ', nombre);
+console.log('edad: ', edad);
+console.log('desarrollador: ', desarrollador);
+console.log('nacimiento: ', nacimiento);
+console.log('libroFav: ', libroFav);
