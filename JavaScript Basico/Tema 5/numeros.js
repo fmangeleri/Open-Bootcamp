6f23c9a2e97915593ff4fa1df5e5fafc0a69f199
fmangeleri <98715817+fmangeleri@users.noreply.github.com>
2022-10-12 14:17:03 -0300

    Add files via upload

---
+++ JavaScript Basico/Tema 5/numeros.js
@@ -0,0 +1,13 @@
+
+let alturaCM = 172
+let alturaM = 1.72
+
+let pesoKG = 76.5
+
+let alturaRedondeado = Math.ceil(alturaM)
+
+let pesoRedondeado = Math.floor(pesoKG)
+
+let maxValue = (Number.MAX_VALUE + 1) === Number.MAX_VALUE
+
+console.log(maxValue)
