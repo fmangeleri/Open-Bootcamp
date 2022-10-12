3f01dd11394fd58f3c8472869037430856b663d3
fmangeleri <98715817+fmangeleri@users.noreply.github.com>
2022-10-12 13:42:38 -0300

    Add files via upload

---
+++ JavaScript Basico/Tema 4/index.js
@@ -0,0 +1,15 @@
+
+let nombre = `Franco`
+let apellido = `Angeleri`
+
+let estudiante = nombre.concat(" ", apellido)
+
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+
+let lenEstudiante = estudiante.length
+let inicialNombre = nombre.charAt(0)
+let ultimaApellido = apellido.charAt(apellido.length - 1)
+
+let estudianteSinEspacios = estudiante.replace(' ', '')
+let nombreEnEstudiante = estudiante.includes(nombre)
