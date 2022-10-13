8320e6f51d028f3e82e271e0bfb3f88c2cc02804
fmangeleri <98715817+fmangeleri@users.noreply.github.com>
2022-10-13 14:06:20 -0300

    Add files via upload

---
+++ JavaScript Basico/Tema 10/index.js
@@ -0,0 +1,8 @@
+import { suma, multiplica } from "./modulos-es6/controller.js"
+
+console.log(suma(1, 2))
+console.log(suma(4, 5))
+
+import chalk from 'chalk';
+
+console.log(chalk.green(suma(4, 5)));
